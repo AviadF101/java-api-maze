fc4e2389c3e846baad05c42a3c784145571a85de
Aviad <aviadf2007@gmail.com>
2026-06-12 00:18:39 +0300

    Restore original lambda thread Main, clean up MazeApp and enforce textfield bounds

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,11 @@
 package org.example;
 
-import javax.swing.SwingUtilities;
-
 public class Main {
     public static void main(String[] args) {
-        // הדרך הרשמית והבטוחה ביותר ב-Java להזניק חלונות גרפיים של Swing
-        SwingUtilities.invokeLater(() -> {
+        new Thread(() -> {
+
             MazeApp app = new MazeApp();
-            app.setVisible(true); // שורת הקסם שמציגה את החלון על המסך!
-        });
+            app.setVisible(true);
+        }).start();
     }
 }
